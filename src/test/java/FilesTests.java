44f7fb69024284efaf460dde0ff55b54588f9e71
Vasili888-QA <vasil88888@inbox.ru>
2021-10-07 19:46:18 +0300

    Added Tests for checking data into files

--- src/test/java/FilesTests.java
+++ src/test/java/FilesTests.java
@@ -1,8 +1,87 @@
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.io.inputstream.ZipInputStream;
+import net.lingala.zip4j.model.LocalFileHeader;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilesTests {
 
     @Test
-    void txtFileTest() {
+    void excelFileTest() throws Exception {
+          try (InputStream stream = getClass().getClassLoader().getResourceAsStream("ExampleExcel.xlsx")) {
+              assert stream != null;
+              XLS parsed = new XLS(stream);
+              assertThat(parsed.excel.getNumberOfSheets()).isEqualTo(1);
+              assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue()).isEqualTo("FirstRow-FirstColumn");
+              assertThat(parsed.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue()).isEqualTo("SecondRow-SecondColumn");
+              assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(2).getStringCellValue()).isEqualTo("FirstRow-ThirdColumn");
+          }
+    }
+
+    @Test
+    void txtFileTest() throws Exception {
+        String result;
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream("ExampleTXTFile.txt")) {
+            assert stream != null;
+            result = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+            assertThat(result).hasLineCount(1);
+            assertThat(result).contains("Создайте свои файлы и напишите проверки содержимого:");
+        }
+    }
+
+    @Test
+    void pdfFileTest() throws Exception {
+        PDF parsed = new PDF(getClass().getClassLoader().getResource("ExamplePDFFile.pdf"));
+        assertThat(parsed.author).contains("Michael Sorens");
+        assertThat(parsed.text).contains("Sprinkled with Selenium usage tips, this is both a general-purpose set of recipes " +
+                "for each technology as well as a cross-reference to map from one to another");
+    }
+
+
+
+    @Test
+    void docxFileTest() throws Exception {
+        File docxFile = new File("src/test/resources/ExampleWord.docx");
+        try (FileInputStream stream = new FileInputStream(docxFile)) {
+            XWPFDocument fileData = new XWPFDocument(stream);
+            XWPFWordExtractor extract = new XWPFWordExtractor(fileData);
+            //Check data into file
+            assertThat(extract.getText()).contains("Word file");
+        }
+    }
+
+    @Test
+    void zipArchiveTest() throws Exception {
+        ZipFile zipFile = new ZipFile("src/test/resources/WorkWithFiles.zip");
+        LocalFileHeader localFileHeader;
+        String password = "test123";
+
+        //Check how many files in archive
+        assertThat(zipFile.getFileHeaders().size()).isEqualTo(138);
+
+        try (InputStream inputStream = new FileInputStream(String.valueOf(zipFile))) {
+            ZipInputStream zipInputStream = new ZipInputStream(inputStream, password.toCharArray());
+            String fileName;
+            ArrayList<String> list = new ArrayList<>();
+            while ((localFileHeader = zipInputStream.getNextEntry()) != null) {
+                File extractedFile = new File(localFileHeader.getFileName());
+                fileName = extractedFile.getName();
+                list.add(fileName);
+            }
+
+            //Check that archive includes .txt file
+            assertThat(list).contains("EXampleTXTFile.txt");
+        }
     }
 }
